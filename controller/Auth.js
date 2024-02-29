69510fb13766785de18893a534f7554658302199
karan <ks888570@gmail.com>
2024-02-29 15:19:48 +0530

    reset password link changed

--- controller/Auth.js
+++ controller/Auth.js
@@ -96,7 +96,7 @@ exports.resetPasswordRequest = async (req, res) => {
 
     // Also set token in email
     const resetPageLink =
-      'http://localhost:3000/reset-password?token=' + token + '&email=' + email;
+      'https://mern-ecom-three.vercel.app/reset-password?token=' + token + '&email=' + email;
     const subject = 'reset password for e-commerce';
     const html = `<p>Click <a href='${resetPageLink}'>here</a> to Reset Password</p>`;
 
