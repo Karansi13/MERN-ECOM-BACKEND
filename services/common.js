cea09be62b10630cb4ea4f8db9363e39b7175e35
karan <ks888570@gmail.com>
2024-02-29 12:59:46 +0530

    completed all todos

--- services/common.js
+++ services/common.js
@@ -27,8 +27,6 @@ exports.cookieExtractor = function(req) {
   if (req && req.cookies) {
       token = req.cookies['jwt'];
   }
-  // temporary   
-  // token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZGEzYTdjZDM5M2IxYmU5ODdlYzU0NCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA4ODAwOTUxfQ.vD_7bWzyEgRckHpQh5VAheluPVyEmfyAroWJADSx1oI";
   return token;
 };
 
@@ -38,7 +36,6 @@ exports.sendMail = async function({ to, subject, text, html }){
     let info = await transporter.sendMail({
       from: '"E-commerce" karansinghk1307@gmail.com>', // sender address
       to: to,
-      // "karansinghk1307@gmail.com", // list of receivers
       subject,
       text,
       html,
